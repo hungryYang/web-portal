8ad2becb500a99311b73f902767e23c5dd4d1278
hungryyang <ycr1714336458@163.com>
2017-11-22 13:50:52 +0800

    simple-mvc

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
+// const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
+const UserService = require('../services/user_service')
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-  res.send('respond with a resource');
+  const users = UserService.getAllUsers()
+  res.locals.users = users
+  res.render('users');
 });
+
+router.post('/', (req, res) => {
+  const {firstName, lastName, age} = req.body;
+  const u = UserService.addNewUser(firstName, lastName, age);
+  res.json(u);
+});
+// `:字符串`会被作为参数处理
+router.get('/:userId', (req, res) => {
+  const user = UserService.getUserById(Number(req.params.userId))
+  res.locals.user = user
+  res.render('user');
+});
+
+router.post('/:userId/subscription', (req, res, next) => {
+  try {
+    const sub = UserService.createSubscription(Number(req.params.userId), req.body.url);
+    res.json(sub);
+  } catch (e) {
+    next(e);
+  }
+});
+
 
 module.exports = router;
