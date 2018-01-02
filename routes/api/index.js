67c7acffcc22ca4becf578a531cdaa27a73a12d5
hungryyang <ycr1714336458@163.com>
2018-01-02 18:48:00 +0800

    JWT token

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,15 +4,22 @@ const router = express.Router();
 
 const userRouter = require('./users');
 
+const JWT = require('jsonwebtoken');
+
 router.get('/login', (req, res, next) => {
   const { username } = req.query;
-  req.session.user = { username };
-  res.send('done');
+  const user = { username };
+  const token = JWT.sign(user, 'asdwdqda');
+  res.send(token);
 });
 
 router.get('/hello', (req, res, next) => {
-  const { username } = req.session.user;
-  res.send(`<h1>hello, ${username}</h1>`);
+  const auth = req.get('Authorization');
+  if (!auth) return res.send('no auth');
+  if (!auth.indexOf('Bearer ') === -1) res.send('no auth');
+  const token = auth.split('Bearer ')[1];
+  const user = JWT.verify(token, 'asdwdqda');
+  res.send(user);
 })
 
 
