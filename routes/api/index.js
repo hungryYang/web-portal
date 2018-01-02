9f730e45e77a64d39e9297de521abffdc7964f8e
hungryyang <ycr1714336458@163.com>
2018-01-02 19:02:34 +0800

    pbkdf2密码加密算法

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,12 +6,36 @@ const userRouter = require('./users');
 
 const JWT = require('jsonwebtoken');
 
-router.get('/login', (req, res, next) => {
-  const { username } = req.query;
-  const user = { username };
-  const token = JWT.sign(user, 'asdwdqda');
-  res.send(token);
-});
+const User = require('../../models/mongoose/user');
+
+const crypto = require('crypto');
+
+// 把经典的callback类型写成Promise
+const pbkdf2Async = require('bluebird').promisify(crypto.pbkdf2);
+
+router.post('/login', (req, res, next) => {
+  (async () => {
+    const { username, password } = req.body;
+    // 加密方法
+    const cipher = await pbkdf2Async(password, 'asdasdasd', 10000, 512, 'sha2656')
+    const created = await User.insert({
+      username,
+      password: cipher,
+    });
+    return created;
+  })()
+    .then((r) => {
+    })
+    .catch((e) => {
+    });
+})
+
+// router.get('/login', (req, res, next) => {
+//   const { username } = req.query;
+//   const user = { username };
+//   const token = JWT.sign(user, 'asdwdqda');
+//   res.send(token);
+// });
 
 router.get('/hello', (req, res, next) => {
   const auth = req.get('Authorization');
